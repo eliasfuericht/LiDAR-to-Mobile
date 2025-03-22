0323e6e960e78a28026ad032c324ef297cede832
Elias Füricht <e52005515@student.tuwien.ac.at>
2025-03-22 17:36:17 +0100

    sending pointxyz to phone

--- raspi-app/src/main.cpp
+++ raspi-app/src/main.cpp
@@ -7,8 +7,10 @@
 #include <cstdlib>
 #include <sstream>
 
+#include "livox_lidar_def.h"
+#include "livox_lidar_api.h"
+
 #include "common.hpp"
-#include "LidarInterface.hpp"
 
 // receiver
 std::string PHONE_IP;
@@ -21,7 +23,7 @@ std::string getPhoneIP() {
   char buffer[128];
   std::string result = "";
 
-  FILE* pipe = popen("ip route | grep default", "r");
+  FILE* pipe = popen("ip route | grep '^default' | grep 'wlan0'", "r");
   if (!pipe) return "ERROR";
 
   while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
@@ -36,64 +38,200 @@ std::string getPhoneIP() {
   return ip;
 }
 
-int i = 0;
-void sendLidarData(int send_sock, struct sockaddr_in& phone_addr) {
-  std::string serializedData = std::to_string(i++);
+void PointCloudCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEthernetPacket* data, void* client_data) {
+  if (data == nullptr) {
+    return;
+  }
+  printf("point cloud handle: %u, data_num: %d, data_type: %d, length: %d, frame_counter: %d\n",
+    handle, data->dot_num, data->data_type, data->length, data->frame_cnt);
+    
+    
+    if (data->data_type == kLivoxLidarCartesianCoordinateHighData) {
+      int32_t buffer_size = data->dot_num * 3;
+      int32_t pos_buffer[buffer_size];
+      int32_t index = 0;
 
-  // Send UDP packet
-  ssize_t sent_bytes = sendto(send_sock, serializedData.c_str(), serializedData.size(), 0,
-                              (struct sockaddr*)&phone_addr, sizeof(phone_addr));
+      LivoxLidarCartesianHighRawPoint *p_point_data = (LivoxLidarCartesianHighRawPoint *)data->data;
+      for (uint32_t i = 0; i < data->dot_num; i++) {
+        pos_buffer[index++] = p_point_data[i].x;
+        pos_buffer[index++] = p_point_data[i].y;
+        pos_buffer[index++] = p_point_data[i].z;
+      }
 
-  if (sent_bytes < 0) {
-    std::cerr << "Failed to send LiDAR data\n";
+      //send pos_buffer
+      ssize_t sent_bytes = sendto(SEND_SOCK, pos_buffer, buffer_size * sizeof(int32_t), 0,
+                                (struct sockaddr*)&PHONE_ADDR, sizeof(PHONE_ADDR));
+      
+      if (sent_bytes < 0) {
+      std::cerr << "Failed to send LiDAR data\n";
+      }
+  }
+  else if (data->data_type == kLivoxLidarCartesianCoordinateLowData) {
+    LivoxLidarCartesianLowRawPoint *p_point_data = (LivoxLidarCartesianLowRawPoint *)data->data;
+  } else if (data->data_type == kLivoxLidarSphericalCoordinateData) {
+    LivoxLidarSpherPoint* p_point_data = (LivoxLidarSpherPoint *)data->data;
   }
 }
 
+void ImuDataCallback(uint32_t handle, const uint8_t dev_type,  LivoxLidarEthernetPacket* data, void* client_data) {
+  if (data == nullptr) {
+    return;
+  } 
+  printf("Imu data callback handle:%u, data_num:%u, data_type:%u, length:%u, frame_counter:%u.\n",
+    handle, data->dot_num, data->data_type, data->length, data->frame_cnt);
+  } 
+  
+  // void OnLidarSetIpCallback(livox_vehicle_status status, uint32_t handle, uint8_t ret_code, void*) {
+    //   if (status == kVehicleStatusSuccess) {
+      //     printf("lidar set ip slot: %d, ret_code: %d\n",
+      //       slot, ret_code);
+      //   } else if (status == kVehicleStatusTimeout) {
+        //     printf("lidar set ip number timeout\n");
+        //   }
+        // }
+        
+        void WorkModeCallback(livox_status status, uint32_t handle,LivoxLidarAsyncControlResponse *response, void *client_data) {
+  if (response == nullptr) {
+    return;
+  }
+  printf("WorkModeCallack, status:%u, handle:%u, ret_code:%u, error_key:%u",
+      status, handle, response->ret_code, response->error_key);
+
+    }
+
+void RebootCallback(livox_status status, uint32_t handle, LivoxLidarRebootResponse* response, void* client_data) {
+  if (response == nullptr) {
+    return;
+  }
+  printf("RebootCallback, status:%u, handle:%u, ret_code:%u",
+      status, handle, response->ret_code);
+}
+
+void SetIpInfoCallback(livox_status status, uint32_t handle, LivoxLidarAsyncControlResponse *response, void *client_data) {
+  if (response == nullptr) {
+    return;
+  }
+  printf("LivoxLidarIpInfoCallback, status:%u, handle:%u, ret_code:%u, error_key:%u",
+    status, handle, response->ret_code, response->error_key);
+    
+    if (response->ret_code == 0 && response->error_key == 0) {
+      LivoxLidarRequestReboot(handle, RebootCallback, nullptr);
+  }
+}
+
+void QueryInternalInfoCallback(livox_status status, uint32_t handle, 
+    LivoxLidarDiagInternalInfoResponse* response, void* client_data) {
+  if (status != kLivoxLidarStatusSuccess) {
+    printf("Query lidar internal info failed.\n");
+    QueryLivoxLidarInternalInfo(handle, QueryInternalInfoCallback, nullptr);
+    return;
+  }
+
+  if (response == nullptr) {
+    return;
+  }
+
+  uint8_t host_point_ipaddr[4] {0};
+  uint16_t host_point_port = 0;
+  uint16_t lidar_point_port = 0;
+  
+  uint8_t host_imu_ipaddr[4] {0};
+  uint16_t host_imu_data_port = 0;
+  uint16_t lidar_imu_data_port = 0;
+  
+  uint16_t off = 0;
+  for (uint8_t i = 0; i < response->param_num; ++i) {
+    LivoxLidarKeyValueParam* kv = (LivoxLidarKeyValueParam*)&response->data[off];
+    if (kv->key == kKeyLidarPointDataHostIpCfg) {
+      memcpy(host_point_ipaddr, &(kv->value[0]), sizeof(uint8_t) * 4);
+      memcpy(&(host_point_port), &(kv->value[4]), sizeof(uint16_t));
+      memcpy(&(lidar_point_port), &(kv->value[6]), sizeof(uint16_t));
+    } else if (kv->key == kKeyLidarImuHostIpCfg) {
+      memcpy(host_imu_ipaddr, &(kv->value[0]), sizeof(uint8_t) * 4);
+      memcpy(&(host_imu_data_port), &(kv->value[4]), sizeof(uint16_t));
+      memcpy(&(lidar_imu_data_port), &(kv->value[6]), sizeof(uint16_t));
+    }
+    off += sizeof(uint16_t) * 2;
+    off += kv->length;
+  }
+  
+  printf("Host point cloud ip addr:%u.%u.%u.%u, host point cloud port:%u, lidar point cloud port:%u.\n",
+    host_point_ipaddr[0], host_point_ipaddr[1], host_point_ipaddr[2], host_point_ipaddr[3], host_point_port, lidar_point_port);
+
+    printf("Host imu ip addr:%u.%u.%u.%u, host imu port:%u, lidar imu port:%u.\n",
+      host_imu_ipaddr[0], host_imu_ipaddr[1], host_imu_ipaddr[2], host_imu_ipaddr[3], host_imu_data_port, lidar_imu_data_port);
+      
+    }
+
+    void LidarInfoChangeCallback(const uint32_t handle, const LivoxLidarInfo* info, void* client_data) {
+      if (info == nullptr) {
+    printf("lidar info change callback failed, the info is nullptr.\n");
+    return;
+  } 
+  printf("LidarInfoChangeCallback Lidar handle: %u SN: %s\n", handle, info->sn);
+  
+  // set the work mode to kLivoxLidarNormal, namely start the lidar
+  SetLivoxLidarWorkMode(handle, kLivoxLidarNormal, WorkModeCallback, nullptr);
+  
+  QueryLivoxLidarInternalInfo(handle, QueryInternalInfoCallback, nullptr);
+  
+  // LivoxLidarIpInfo lidar_ip_info;
+  // strcpy(lidar_ip_info.ip_addr, "192.168.1.10");
+  // strcpy(lidar_ip_info.net_mask, "255.255.255.0");
+  // strcpy(lidar_ip_info.gw_addr, "192.168.1.1");
+  // SetLivoxLidarLidarIp(handle, &lidar_ip_info, SetIpInfoCallback, nullptr);
+}
+
+void LivoxLidarPushMsgCallback(const uint32_t handle, const uint8_t dev_type, const char* info, void* client_data) {
+  struct in_addr tmp_addr;
+  tmp_addr.s_addr = handle;  
+  std::cout << "handle: " << handle << ", ip: " << inet_ntoa(tmp_addr) << ", push msg info: " << std::endl;
+  std::cout << info << std::endl;
+  return;
+}
 
 int main(int argc, const char *argv[]) {
+    
+    PHONE_IP = getPhoneIP();
+    std::cout << "Detected Default Gateway(PHONE): " << PHONE_IP << std::endl;
+    
+    // Setting up UDP data transmission to mobile device
+    SEND_SOCK = socket(AF_INET, SOCK_DGRAM, 0);
+    if (SEND_SOCK < 0) {
+      std::cerr << "Error creating UDP socket\n";
+      return 1;
+    }
+    
+    PHONE_ADDR.sin_family = AF_INET;
+    PHONE_ADDR.sin_port = htons(PHONE_PORT);
+    inet_pton(AF_INET, PHONE_IP.c_str(), &PHONE_ADDR.sin_addr);
+    
+    // Sending data
+    std::cout << "Sending data to " << PHONE_IP << " via port " << PHONE_PORT << std::endl;
   
-  PHONE_IP = getPhoneIP();
-  std::cout << "Detected Default Gateway(PHONE): " << PHONE_IP << std::endl;
+    // Setting up Livox Mid360
+    // config file has to be in the same dir as executable
+    const std::string path = "config.json";
   
-  // Setting up UDP data transmission to mobile device
-  SEND_SOCK = socket(AF_INET, SOCK_DGRAM, 0);
-  if (SEND_SOCK < 0) {
-    std::cerr << "Error creating UDP socket\n";
-    return 1;
-  }
-  
-  PHONE_ADDR.sin_family = AF_INET;
-  PHONE_ADDR.sin_port = htons(PHONE_PORT);
-  inet_pton(AF_INET, PHONE_IP.c_str(), &PHONE_ADDR.sin_addr);
-  
-  // Sending data
-  std::cout << "Sending data to " << PHONE_IP << " via port " << PHONE_PORT << std::endl;
-  
-  // Setting up Livox Mid360
-  // config file has to be in the same dir as executable
-  const std::string path = "config.json";
-
-  // init SDK
-  if (!LivoxLidarSdkInit(path.c_str())) {
-    printf("Livox Init Failed\n");
+    // init SDK
+    if (!LivoxLidarSdkInit(path.c_str())) {
+      printf("Livox Init Failed\n");
+      LivoxLidarSdkUninit();
+      return -1;
+    }
+    
+    SetLivoxLidarPointCloudCallBack(PointCloudCallback, nullptr);
+    
+    SetLivoxLidarImuDataCallback(ImuDataCallback, nullptr);
+    
+    SetLivoxLidarInfoCallback(LivoxLidarPushMsgCallback, nullptr);
+    
+    SetLivoxLidarInfoChangeCallback(LidarInfoChangeCallback, nullptr);
+    
+    sleep(300);
+    
+    // cleanup
     LivoxLidarSdkUninit();
-    return -1;
+    close(SEND_SOCK);
+    return 0;
   }
-  
-  // REQUIRED, to get point cloud data via 'PointCloudCallback'
-  SetLivoxLidarPointCloudCallBack(PointCloudCallback, nullptr);
-  
-  // OPTIONAL, to get imu data via 'ImuDataCallback'
-  // some lidar types DO NOT contain an imu component
-  SetLivoxLidarImuDataCallback(ImuDataCallback, nullptr);
-  
-  SetLivoxLidarInfoCallback(LivoxLidarPushMsgCallback, nullptr);
-  
-  // REQUIRED, to get a handle to targeted lidar and set its work mode to NORMAL
-  SetLivoxLidarInfoChangeCallback(LidarInfoChangeCallback, nullptr);
-
-  // cleanup
-  LivoxLidarSdkUninit();
-  close(SEND_SOCK);
-  return 0;
-}
