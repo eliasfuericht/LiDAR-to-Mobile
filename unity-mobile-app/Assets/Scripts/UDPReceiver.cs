d7ff1f197123b7a6ac9b65406e92a3ce0b33d568
Elias Füricht <eliafuer@gmail.com>
2025-04-26 14:17:07 +0200

    unity visualizer working
    need to fix camera orbit

--- unity-mobile-app/Assets/Scripts/UDPReceiver.cs
+++ unity-mobile-app/Assets/Scripts/UDPReceiver.cs
@@ -1,22 +1,45 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using UnityEngine;
+using System.Collections.Generic;
+using static UnityEngine.ParticleSystem;
 
 public class UDPReceiver : MonoBehaviour
 {
     private UdpClient udpClient;
     private Thread receiveThread;
     private const int listenPort = 8888;
-    public PointCloudVisualizer visualizer;
-    private int m_counter = 0;
+
+    private ParticleSystem particleSystem;
+    private static int maxPointCount = 19968;
+    private ParticleSystem.Particle[] particles = new ParticleSystem.Particle[maxPointCount];
+    private int index = 0;
+
+    void Awake()
+    {
+        particleSystem = gameObject.AddComponent<ParticleSystem>();
+
+        var main = particleSystem.main;
+        main.loop = false;
+        main.playOnAwake = false;
+        main.startLifetime = Mathf.Infinity;
+        main.startSpeed = 0f;
+        main.maxParticles = maxPointCount;
+        main.simulationSpace = ParticleSystemSimulationSpace.World;
+
+        var emission = particleSystem.emission;
+        emission.enabled = false;
+
+        var shape = particleSystem.shape;
+        shape.enabled = false;
+    }
 
     void Start()
     {
         udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, listenPort));
-        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread = new Thread(ReceiveData);
         receiveThread.IsBackground = true;
         receiveThread.Start();
         Debug.Log($"UDPReceiver started on port {listenPort}");
@@ -34,25 +57,29 @@ public class UDPReceiver : MonoBehaviour
                 if (data.Length != 0)
                 {
                     int numInts = data.Length / sizeof(int);
-                    
-                    m_counter += numInts;
-
                     int numPoints = numInts / 3;
-                    
-                    Vector3Int[] pointData = new Vector3Int[numPoints];
 
+                    if (index > maxPointCount - numPoints)
+                    {
+                        index = 0;
+                    }
+                    
                     for (int i = 0; i < numPoints; i++)
                     {
                         int baseIndex = i * 3 * sizeof(int);
 
-                        int x = BitConverter.ToInt32(data, baseIndex);
-                        int y = BitConverter.ToInt32(data, baseIndex + sizeof(int));
-                        int z = BitConverter.ToInt32(data, baseIndex + 2 * sizeof(int));
+                        float x = BitConverter.ToInt32(data, baseIndex) / 100.0f;
+                        float y = BitConverter.ToInt32(data, baseIndex + sizeof(int)) / 100.0f;
+                        float z = BitConverter.ToInt32(data, baseIndex + 2 * sizeof(int)) / 100.0f;
 
-                        pointData[i] = new Vector3Int(x, y, z);
+                        particles[index].position = new Vector3(x, y, z);
+                        particles[index].startSize = 0.1f;
+                        particles[index].startColor = Color.white;
+                        particles[index].remainingLifetime = Mathf.Infinity;
+                        index++;
                     }
-                    // visualizing is the bottleneck rn
-                    
+
+                    particleSystem.SetParticles(particles, maxPointCount);
                 }
             }
         }
