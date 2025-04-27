91a11b393667931f8b7ba6334e99b074ac5645d6
Elias Füricht <e52005515@student.tuwien.ac.at>
2025-04-27 22:28:58 +0200

    cleanup + fixed odd buffers spilling elements

--- raspi-app/src/main.cpp
+++ raspi-app/src/main.cpp
@@ -1,15 +1,7 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <cstring>
-#include <cstdlib>
 #include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <thread>
-#include <chrono>
 #include <iostream> 
 #include <fstream>
 #include <cmath>
@@ -19,23 +11,22 @@
 #include "livox_lidar_def.h"
 #include "livox_lidar_api.h"
 
-enum DataFlags
-{
-  FLAG_POINT_DATA = 0,
-  FLAG_IMU_DATA = 1
-};
+// split up buffer into smaller ones if this number is exceeded
+// leave space for udp headers and custom flag 
+#define MAX_UDP_BUFFER_BYTES 65503
+
+// flags for datatransfer
+#define FLAG_POINT_DATA 0
+#define FLAG_IMU_DATA 1
 
 // variables for point data accumulation
-// split up buffer into smaller ones if this number is exceeded
-// leave space for udp headers and custom flag
-#define MAX_UDP_BUFFER_BYTES 65503
 // sensor sends 2083 packages @ 96 vertices per second (200000 points/s)
 const int NUM_TOTAL_PACKAGES_PER_SECOND = 2083;
 // NUM_POINTS_PER_PACKAGE: Number of points received from sensor per package (x,y,z)
 const int NUM_POINTS_PER_PACKAGE = 96;
 
 // NUM_PACKAGES: Number of point-data-packages (UDP packages) that get accumulated
-int s_num_packages = 1;
+int s_num_packages;
 // TOTAL_NUM_POINTS: Number of points accumulated 
 int s_total_num_points;
 // BUFFER_SIZE: Size of buffer preallocated
@@ -44,14 +35,14 @@ int s_buffer_num_elements;
 int32_t s_package_counter = 0;
 // keeps track of index into buffer
 int32_t s_buffer_index = 0;
-
+// point data
 std::vector<int32_t> s_pos_buffer;
 
 // IMU data
-LivoxLidarImuRawPoint s_current_imu_data;
+LivoxLidarImuRawPoint s_imu_data;
 
 // writing data to disk
-bool s_write_pointcloud_to_disk = false;
+bool s_write_data_to_disk = false;
 int s_disc_data_counter = 0;
 
 // udp receiver data
@@ -60,6 +51,7 @@ int s_phone_port = 8888;
 struct sockaddr_in s_phone_adress = {};
 int s_send_sock;
 
+// helperfunction - getting ip of standardgateway (if connected with hotspot of phone -> will be phones ip)
 std::string getPhoneIP() {
   char buffer[128];
   std::string result = "";
@@ -79,6 +71,7 @@ std::string getPhoneIP() {
   return ip;
 }
 
+// function to write point data to disk as .ply
 void WriteToDisk()
 {
   std::ostringstream filename;
@@ -109,10 +102,12 @@ void WriteToDisk()
 
 void SendDataToMobile()
 {
+  // calculate size of buffer to be sent
   int32_t current_udp_bytes = s_buffer_num_elements * sizeof(int32_t);
 
   ssize_t sent_bytes = -1;
 
+  // buffer to hold flag + positionbuffer
   std::vector<int32_t> flag_prepended_buffer;
 
   // if current buffer is too big, subdivide buffer
@@ -120,6 +115,9 @@ void SendDataToMobile()
   {
     // calculate how many buffers are needed
     int32_t num_split_buffers = std::ceil((float)current_udp_bytes / (float)MAX_UDP_BUFFER_BYTES);
+
+    // increment if num_split_buffers is odd
+    num_split_buffers += (num_split_buffers % 2);
 
     // calculate how many elements (one element is one component of a point, so x or y or z) per buffer
     int32_t num_elements_per_buffer = s_buffer_num_elements / num_split_buffers;
@@ -129,10 +127,12 @@ void SendDataToMobile()
 
     flag_prepended_buffer.resize(num_elements_per_buffer + 1);
 
+    // prepend flag
     flag_prepended_buffer[0] = FLAG_POINT_DATA;
 
     for (int i = 0; i < num_split_buffers; i++)
     {
+      // memcpy bufferpart
       std::memcpy(flag_prepended_buffer.data() + 1, s_pos_buffer.data() + i * num_elements_per_buffer, current_udp_bytes);
       sent_bytes = sendto(s_send_sock, flag_prepended_buffer.data(), flag_prepended_buffer.size() * sizeof(int), 0, (struct sockaddr*)&s_phone_adress, sizeof(s_phone_adress));
     }
@@ -167,8 +167,6 @@ void PointCloudCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEther
     int32_t num_points = data->dot_num;
     
     LivoxLidarCartesianHighRawPoint *p_point_data = (LivoxLidarCartesianHighRawPoint *)data->data;
-
-    // integrate s_current_imu_data here somewhere?
     
     // fill buffer with elements
     for (uint32_t i = 0; i < num_points; i++) 
@@ -178,7 +176,7 @@ void PointCloudCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEther
       s_pos_buffer[s_buffer_index++] = p_point_data[i].z;
     }
 
-    // send data as soon as NUM_PACKAGES is reached
+    // send data as soon as s_num_packages is reached
     if (s_package_counter % s_num_packages == 0)
     {
       // reset index into buffer
@@ -186,10 +184,9 @@ void PointCloudCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEther
 
       SendDataToMobile();
       
-      if (s_write_pointcloud_to_disk)
+      if (s_write_data_to_disk)
       {
         WriteToDisk();
-        std::cout << "stop" << std::endl;
       }
     }
   }
@@ -205,20 +202,22 @@ void ImuDataCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEthernet
   if (data->data_type == kLivoxLidarImuData) 
   {
     LivoxLidarImuRawPoint *imu_data = (LivoxLidarImuRawPoint *)data->data;
-    s_current_imu_data = *imu_data;
+    s_imu_data = *imu_data;
 
     std::vector<float> flag_prepended_buffer;
 
+    // hardcoded since IMU layout isn't dynamic
     flag_prepended_buffer.resize(6 + 1);
 
+    // set flag
     *reinterpret_cast<int*>(&flag_prepended_buffer[0]) = FLAG_IMU_DATA;
 
-    flag_prepended_buffer[1] = s_current_imu_data.acc_x;
-    flag_prepended_buffer[2] = s_current_imu_data.acc_y;
-    flag_prepended_buffer[3] = s_current_imu_data.acc_z;
-    flag_prepended_buffer[4] = s_current_imu_data.gyro_x;
-    flag_prepended_buffer[5] = s_current_imu_data.gyro_y;
-    flag_prepended_buffer[6] = s_current_imu_data.gyro_z;
+    flag_prepended_buffer[1] = s_imu_data.acc_x;
+    flag_prepended_buffer[2] = s_imu_data.acc_y;
+    flag_prepended_buffer[3] = s_imu_data.acc_z;
+    flag_prepended_buffer[4] = s_imu_data.gyro_x;
+    flag_prepended_buffer[5] = s_imu_data.gyro_y;
+    flag_prepended_buffer[6] = s_imu_data.gyro_z;
 
     ssize_t sent_bytes = sendto(s_send_sock, flag_prepended_buffer.data(), flag_prepended_buffer.size() * sizeof(float), 0, (struct sockaddr*)&s_phone_adress, sizeof(s_phone_adress));
   
@@ -229,6 +228,7 @@ void ImuDataCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEthernet
   }
 }
 
+// helper function for input parameters
 bool is_number(const std::string& s) 
 {
   for (char const& ch : s) 
@@ -238,17 +238,15 @@ bool is_number(const std::string& s)
   return true;
 }
 
-int main(int argc, const char *argv[]) 
+// Calculate data per package if accumulation is active
+void SetupTransfereRate(float update_rate)
 {
-  if (argc != 2 || !is_number(argv[1]) || std::atof(argv[1]) < 0) 
+  // set to insta send packages if 0
+  if (update_rate == 0)
   {
-    printf("Params Invalid, please specify the data rate in milliseconds (integer).\n");
-    return -1;
+    s_num_packages = 1;
   }
-  
-  float update_rate = std::atof(argv[1]);
-
-  if (update_rate != 0)
+  else // calc number of packages to accumulate
   {
     float data_rate = update_rate / 1000.0f;
   
@@ -260,30 +258,58 @@ int main(int argc, const char *argv[])
   s_buffer_num_elements = s_total_num_points * 3;
   s_pos_buffer.resize(s_buffer_num_elements);
 
-  std::cout << s_num_packages << " packages will be cummulated and a total of " 
-            << s_total_num_points << " will be sent every " << update_rate << " milliseconds." << std::endl;
+  if (s_num_packages > 1)
+  {
+    std::cout << s_num_packages << " packages will be cumulated and a total of " 
+              << s_total_num_points << " will be sent every " << update_rate << " milliseconds." << std::endl;
+  }
+  else
+  {
+    std::cout << "forwarding packages instantly without accumulation." << std::endl;
+  }
+}
 
+// Setting up UDP data transmission to mobile device
+bool SetupUDP()
+{
   s_phone_IP = getPhoneIP();
   
-  // Setting up UDP data transmission to mobile device
   s_send_sock = socket(AF_INET, SOCK_DGRAM, 0);
-  if (s_send_sock < 0) {
+  if (s_send_sock < 0) 
+  {
     std::cerr << "Error creating UDP socket\n";
-    return 1;
+    return false;
   }
   
   s_phone_adress.sin_family = AF_INET;
   s_phone_adress.sin_port = htons(s_phone_port);
   inet_pton(AF_INET, s_phone_IP.c_str(), &s_phone_adress.sin_addr);
   
-  // Sending data
   std::cout << "Sending data to " << s_phone_IP << " via port " << s_phone_port << std::endl;
+  return true;
+}
+
+int main(int argc, const char *argv[]) 
+{
+  if (argc != 2 || !is_number(argv[1]) || std::atof(argv[1]) < 0) 
+  {
+    std::cout << "Params Invalid, please specify the data rate in milliseconds (integer)." << std::endl;
+    return -1;
+  }
+  
+  SetupTransfereRate(std::atof(argv[1]));
+
+  if (!SetupUDP())
+  {
+    std::cerr << "Error setting up UDP\n";
+    return 1;
+  }
 
   // Setting up Livox Mid360
   // init SDK
   // config file has to be in the same dir as executable
   if (!LivoxLidarSdkInit("receiver-config.json")) {
-    printf("Livox Init Failed\n");
+    std::cout << "Livox Init Failed\n" << std::endl;
     LivoxLidarSdkUninit();
     return -1;
   }
